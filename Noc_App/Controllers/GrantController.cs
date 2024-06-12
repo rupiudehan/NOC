576369f791705d0109b8154351cbdf68988a1a58
rupiudehan <135786909+rupiudehan@users.noreply.github.com>
2024-06-12 16:01:33 +0530

    tt

--- Noc_App/Controllers/GrantController.cs
+++ Noc_App/Controllers/GrantController.cs
@@ -864,7 +864,11 @@ namespace Noc_App.Controllers
                                             count++;
                                             area = area - 1;
                                         } while (area > 0);
-                                    additionalcalculation = "Additional Area Calculation With Amount ₹" + 250.ToString() + "per/Acre : On Additional " + Convert.ToDouble(area2)<0 ? (Convert.ToDouble(area2) * -1).ToString(): area2 + " Acres, Amount is ₹" + (TotalPayment + (count * 250) - 1000).ToString();
+                                    if(Convert.ToDouble(area2) < 0)
+                                    {
+                                        area2=(Convert.ToDouble(area2) * -1).ToString()
+                                    }
+                                    additionalcalculation = "Additional Area Calculation With Amount ₹" + 250.ToString() + "per/Acre : On Additional " + area2 + " Acres, Amount is ₹" + (TotalPayment + (count * 250) - 1000).ToString();
                                         TotalPayment = TotalPayment + (count * 250);
                                     totalareacalculation = "Amount Calculation for Total " + model.TotalArea.ToString() + " Acres : ₹" + TotalPayment.ToString()+" (NON-REFUNDABLE)";
                                     }
