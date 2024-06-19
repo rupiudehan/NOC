f3afeb1355b725443276c3fb6ebe1b9b3e488437
rupiudehan <135786909+rupiudehan@users.noreply.github.com>
2024-06-19 12:04:48 +0530

    scripts are separated

---
+++ Noc_App/wwwroot/ModuleJs/appforwardscript.js
@@ -0,0 +1,30 @@
+﻿function populateDropdown(data, dropdownId) {
+    var dropdown = $("#Selected" + dropdownId);
+    dropdown.removeAttr("disabled");
+    dropdown.html('<option value="">Select</option>');
+    $.each(data, function (key, value) {
+        dropdown.append('<option value="' + value.value + '">' + value.text + '</option>');
+    });
+}
+$(document).ready(function () {
+    $("#SelectedSubDivisionId").change(function () {
+        var subdivisionId = $(this).val();
+        var role = $('#ForwardToRole').val();
+        console.log(role);
+        $.ajax({
+            url: "/ApprovalProcess/GetOfficers",
+            type: "POST",
+            data: { subdivisionId: subdivisionId, roleName: role },
+            async: false,
+            success: function (data) {
+                populateDropdown(data, "OfficerId");
+            },
+            failure: function (f) {
+                alert(f);
+            },
+            error: function (e) {
+                alert('Error ' + e);
+            }
+        });
+    });
+});
---
+++ Noc_App/wwwroot/ModuleJs/approvallist.js
@@ -0,0 +1,51 @@
+﻿function SetValue(id) {
+    $('#id').val(id);
+    var body = $('#tbRecommendation');
+    var table = $("#tblRecommendation");
+    $('.barcontainer').hide();
+    $("#myModal").hide();
+    body.empty();
+    $("#divLoader").show(); $('.barcontainer').hide();
+    $.ajax({
+        url: "/ApprovalProcess/GetRecommendationDetail",
+        type: "POST",
+        data: { id: id },
+        complete: function (r) {
+            $("#myModal").modal(); $("#myModal").addClass('show');
+            $("#divLoader").hide();
+            $('.barcontainer').show();
+
+        },
+        success: function (r) {
+            if (r != null) {
+                var count = 1; var len = r.length;
+
+                $.each(r, function (key, value) {
+                    var tr = '<tr>';
+                    tr += '<td>' + count + '</td>';
+                    tr += '<td>' + value.applicationId + '</td>';
+                    tr += '<td>' + value.recommended + '</td>';
+                    tr += '<td>' + value.remarks + '</td>';
+                    tr += '<td>' + value.recommendedBy + '</td>';
+                    tr += '<td>' + value.recommendedTo + '</td>';
+                    tr += '</tr>';
+                    body.append(tr);
+                    count++;
+                });
+                table.DataTable();
+            }
+        },
+        failure: function (r) {
+            alert(r.d);
+        },
+        error: function (r) {
+            alert(r.d);
+        }
+    });
+
+}
+$(document).ready(function () {
+    if ($('.table tbody tr').length > 0) {
+        $('.table').DataTable();
+    }
+});
