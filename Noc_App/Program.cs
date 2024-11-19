15413dab6ffb80b9c227f89872030c7a0710d7fd
rupiudehan <135786909+rupiudehan@users.noreply.github.com>
2024-11-19 11:10:28 +0530

    added images restriction

--- Noc_App/Program.cs
+++ Noc_App/Program.cs
@@ -158,7 +158,7 @@ app.Use((context, next) =>
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.google.com https://cdnjs.cloudflare.com https://www.gstatic.com https://cdn.datatables.net https://code.highcharts.com/highcharts.js https://code.highcharts.com/modules/exporting.js https://code.highcharts.com/modules/export-data.js https://code.highcharts.com/modules/accessibility.js; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://www.gstatic.com; " +
-    "img-src 'self' data: http://localhost:5126/ApprovalProcess/Download http://49.50.66.74/ApprovalProcess/Download; " +
+    "img-src 'self' data: http://localhost:5126/ApprovalProcess/Download http://49.50.66.74/ApprovalProcess/Download http://49.50.66.74/lib http://49.50.66.74/img http://49.50.66.74/images http://49.50.66.74/LandingPage/assets/img http://49.50.66.74/LandingPage/assets2/img; " +
     "font-src 'self' https://fonts.gstatic.com data:; " +
     "connect-src 'self' https://api.example.com http://49.50.66.74 http://localhost:44322 http://localhost:44132 ws://localhost:15178 ws://localhost:44352; " +
     "object-src 'none'; " +
